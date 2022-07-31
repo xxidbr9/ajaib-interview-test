ede53a16db63509206bb7f55e5ef2d07f184f38d
Barnando Akbarto Hidayatullah <barnando13@gmail.com>
2022-07-31 17:24:47 +0700

    ci: implement jest for testing

---
+++ src/components/hello-world/hello_world.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import HelloWorld from './hello_world';
+
+describe('testing Hello World', () => {
+  it('Test is render correctly', () => {
+    render(<HelloWorld name="nando" />);
+    screen.findByText('Hello nando');
+  });
+});
