7d5ec10245e563b63df4c685f17586b49f001ff9
Barnando Akbarto Hidayatullah <barnando13@gmail.com>
2022-07-31 14:48:15 +0700

    build: adding production build

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
+import "./assets/css/global.css"
+
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-
-
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const container = document.getElementById('magic');
